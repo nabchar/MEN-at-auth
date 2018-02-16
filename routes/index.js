c7167c40e609cbb5219c0865b0b016b0ba5e3e5c
Nicholas Char <nick.char.nick@gmail.com>
2018-02-16 14:35:51 -0500

    switch it up

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import path from 'path';
-
-const router = express.Router();
-
-router.get('/', (req, res) => res.sendFile(path.join(__dirname, '/template/index.html')));
-
-
-export default router;
